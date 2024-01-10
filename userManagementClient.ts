6735dd70d00ecf69aab2b9dc249176346d3ee5a0
preitinger <peter.reitinger@gmail.com>
2024-01-10 18:44:39 +0100

    client functions for login and logout

--- userManagementClient.ts
+++ userManagementClient.ts
@@ -1,8 +1,24 @@
+import { LoginReq, LoginResp } from "../user-management-server/user-management-common/login";
+import { LogoutReq, LogoutResp } from "../user-management-server/user-management-common/logout";
 import { ApiResp } from "./user-management-common/apiRoutesCommon";
 import { RegisterReq, RegisterResp } from "./user-management-common/register";
 
 export function userRegisterFetch(req: RegisterReq): Promise<ApiResp<RegisterResp>> {
     return fetch('/api/user/register', {
+        method: 'POST',
+        body: JSON.stringify(req)
+    }).then(resp => resp.json())
+}
+
+export function userLoginFetch(req: LoginReq): Promise<ApiResp<LoginResp>> {
+    return fetch('/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify(req)
+    }).then(resp => resp.json())
+}
+
+export function userLogoutFetch(req: LogoutReq): Promise<ApiResp<LogoutResp>> {
+    return fetch('/api/user/logout', {
         method: 'POST',
         body: JSON.stringify(req)
     }).then(resp => resp.json())
